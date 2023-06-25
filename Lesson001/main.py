27176987e994362fbbfe309e65f00eb7bd8ec72d
rusakova slavyana <blondi_sl@mail.ru>
2023-06-25 10:15:27 +0300

    Семинар-1

---
+++ Lesson001/main.py
@@ -0,0 +1,82 @@
+# print(5, 6, 9)
+
+# n = 10
+# print(n)
+# n = "ygdjcjsdbc"
+# print(n)
+
+# n = "hvhgvhg"
+# print(type(n))
+
+# print("Введите значение a:")
+# a = int(input())
+# b = int(input("Введите значение b: "))
+# print(f"{a} + {b} = {a + b}")
+
+# a = 7.66677
+# b = 4.75576576
+# print(round(a*b, 3))
+
+# a = 1>4
+# print(a)
+# a = 1<5 and 4<8
+# print(a)
+# a = 1==5
+# print(a)
+# a = 1 != 2
+# print(a)
+# a = "gvhgf"
+# b = "ghgf"
+# print(a == b)
+# a = 1<6>3>1
+# print(a)
+
+# username = input ("Введите ваше имя: ")
+# if username == "Маша":
+#     print("Ура, это Маша")
+# elif username == "Матвей":
+#     print("Привет, Матвей")
+# else:
+#     print("Привет,", username)
+
+# i = 0
+# while i < 5:
+#     # if i ==3:
+#     #     break
+#     i +=1
+# else:
+#     print("Пожалуй")
+#     print("ХВАТИТ")
+# print(i)
+
+# i = "hello"
+# for a in i:
+#     print(a)
+
+# line = ""
+# for i in range(5):
+#     line = ""
+#     for j in range(5):
+#         line += "*  "
+#     print(line)
+
+# text = "Съешь ЕЩЕ мЯгКих французских БуЛочеК"
+# print(len(text))
+# print("еще" in text)
+# print(text.lower())
+# print("еще" in text.lower())
+# print(text.upper())
+# print(text.replace("ЕЩЕ", "еще"))
+# print(text[0])
+# print(text[1])
+# print(text[len(text)-1])
+# print(text[-1])
+# print(text[:])
+# print(text[:2])
+# print(text[7:15])
+# print(text[::6])
+# text = text[6:9] + text[-5] + text[31:]
+# print(text)
+
+
+
