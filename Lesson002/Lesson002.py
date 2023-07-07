7ce59f29e865236c877ca2dda5c5ce18e1310188
rusakova slavyana <blondi_sl@mail.ru>
2023-07-07 21:44:19 +0300

    Лекция-2

---
+++ Lesson002/Lesson002.py
@@ -0,0 +1,89 @@
+# list = []
+# print(list)
+# for i in range(5):
+#     list.append(i)
+#     print(list)
+
+# list = [1, 4, 44, 4444, 444444]
+# print(list)
+# print(list.pop(3))
+# print(list)
+
+# list = [1, 4, 44, 4444, 444444]
+# print(list)
+# print(list.insert(3, 65654))
+# print(list)
+
+# list_1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(list_1[0]) # 1
+# print(list_1[1]) # 2
+# print(list_1[len(list_1)-1]) # 10
+# print(list_1[-5]) # 6
+# print(list_1[:]) # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# print(list_1[:2]) # [1, 2]
+# print(list_1[len(list_1)-2:]) #[9, 10]
+# print(list_1[2:9]) # [3, 4, 5, 6, 7, 8, 9]
+# print(list_1[6:-18]) # []
+# print(list_1[0:len(list_1):6]) # [1, 7]
+# print(list_1[::6])
+
+# t = ()
+# print(type(t))
+# t = (1, 5, 5)
+# print(type(t))
+
+# t = [1, 4, 55]
+# print(t)
+# print(type(t))
+
+# t = tuple(t)
+# print(t)
+# print(type(t))
+
+# a, b, c = t
+# print(a, b, c)
+
+# t = (1, 3, 55, 44, 33)
+# for i in t:
+#     print(i)
+
+
+# t = (1, 3, 55, 44, 33)
+# t[0] = 66
+
+# t = {}
+# t = dict()
+# t["y"] = "qwerty"
+# print(t)
+
+# t["w"] = "qwert"
+# print(t)
+# print(t["y"])
+
+# dictionary = {}
+# dictionary = {"pppppp" : "p", "jjjjjjj" : "j", "ttttt" : "t", "eeeeeee" : "e"}
+# print(dictionary)
+# print(dictionary["pppppp"])
+# dictionary[666] = 666666666666
+# print(dictionary)
+# del dictionary["eeeeeee"]
+# print(dictionary)
+# for (k, v) in dictionary.items():
+#     print(k, v)
+# print(dictionary.items())
+
+# colors = {"red", "green", "blue"}
+# # print(colors)
+# # colors.add("blue")
+# print(colors)
+# colors.add("grey")
+# print(colors)
+# colors.remove("grey")
+# print(colors)
+# colors.discard("grey")
+# print(colors)
+# colors.clear()
+# print(colors)
+
+# q = set()
+
